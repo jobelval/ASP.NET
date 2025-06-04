102efdf8c006be8541287d7f3e7538d8c55774f6
jobelval <165405839+jobelval@users.noreply.github.com>
2025-06-04 06:43:17 -0400

    Add files via upload

--- formulaireverifier.php
+++ formulaireverifier.php
@@ -15,7 +15,7 @@
         <header>
         <nav class="navbar navbar-expand-lg navbar-light" id="nav">
   <div class="container-fluid">
-    <a class="navbar-brand" href="index.php">Acceuil</a>
+    <a class="navbar-brand" href="index.html">Acceuil</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
